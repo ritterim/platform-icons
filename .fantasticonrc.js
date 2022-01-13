d58c2328b85cb857ff3cce5e377449d265485fe1
Ryan Trimble <mrtrimble2@gmail.com>
2022-01-13 15:32:58 -0500

    cleared git and re-copied things over

---
+++ .fantasticonrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  inputDir: './src/optimized-icons', // (required)
+  outputDir: './src/generated', // (required)
+  fontTypes: ['ttf', 'woff', 'woff2'],
+  assetTypes: ['scss'],
+  prefix: 'pi',
+  name: "platform-icons"
+};
